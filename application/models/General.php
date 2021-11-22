a77a5c174b27f1de8ec91dde6ce32b878fc922e4
MilzamHutomo <milzamhutomo.social@gmail.com>
2021-11-23 01:23:13 +0700

    Fix bug

--- application/models/General.php
+++ application/models/General.php
@@ -558,9 +558,7 @@ class General extends CI_Model
 						$this->db->update('employment_absences');
 					}
 				} else {
-					array_push($data, array('created_at' => date('Y-m-d')));
-					var_dump($data);
-					die;
+					$data['created_at'] = date('Y-m-d');
 					$this->db->insert('employment_absences', $data);
 				}
 			}
