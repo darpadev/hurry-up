4b52215554d12775e7046d0ff3354c705904832c
MilzamHutomo <milzamhutomo.social@gmail.com>
2021-11-22 09:43:49 +0700

    Fix bug

--- application/models/General.php
+++ application/models/General.php
@@ -544,7 +544,7 @@ class General extends CI_Model
 					'receiver'		=> $person->id
 				);
 				
-				if ($this->db->select('*')->from('employment_absences')->where($data)->where('checked', TRUE)->get()->num_rows() > 0){
+				if ($this->db->get_where('employment_absences', $data)->num_rows() > 0){
 					$this->db->set('checked', FALSE);
 					$this->db->where('receiver', $this->session->userdata('id'));
 					$this->db->update('employment_absences');
